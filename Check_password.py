da315691110a978237d1f6e04cb5c3bb286b3e94
User Name <user@example.com>
2025-11-26 20:15:57 +0530

    added In DevOps, security is a crucial aspect, and ensuring strong passwords is essential. Create a Python script to check the strength of the password

---
+++ Check_password.py
@@ -0,0 +1,28 @@
+import re
+
+def check_password_strength(password: str) -> bool:
+    if len(password) < 8:
+        return False
+    if not re.search(r"[A-Z]", password):  #one uppercase
+        return False
+    if not re.search(r"[a-z]", password):  #lowercase
+        return False
+    if not re.search(r"[0-9]", password):  #one digit
+        return False
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):  #one special character
+        return False
+    return True
+
+
+# --- Main Script ---
+if __name__ == "__main__":
+    user_password = input("Enter a password to check its strength: ")
+
+    if check_password_strength(user_password):
+        print("Your password is strong!")
+    else:
+        print("Your password is weak. Make sure it meets the following criteria")
+        print("At least 8 characters long")
+        print("Contains uppercase and lowercase letters")
+        print("Contains at least one number (0-9)")
+        print("Contains at least one special character (!, @, #, $, %, etc.)")
